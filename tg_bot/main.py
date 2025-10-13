38a044ce67b92be94499dd133df378d45a3891fc
Rostislaw Chernyahovskiy <r.chernyahovskll@gmail.com>
2025-10-13 22:45:41 +0300

    Created hendlers file and cmd_start moved to it file

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,18 +1,15 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher
 import json
+from hendlers import router
 
 logging.basicConfig(level=logging.INFO)
 with open(r'C:\Users\rcher\VsCode\tg-web-app\tg_bot\.config.json', 'r') as config_file:
     config_data = json.load(config_file)
     bot = Bot(token=config_data["bot_token"])
 dp = Dispatcher()
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}")
+dp.include_router(router)
 
 async def main():
     await dp.start_polling(bot)
