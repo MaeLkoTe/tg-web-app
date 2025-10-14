9e577d96e3fdc9d492279d6c885b10af1cb3d7fa
Rostislaw Chernyahovskiy <r.chernyahovskll@gmail.com>
2025-10-14 20:25:49 +0300

    Add inline_keyboard with web app at cmd_start func

--- tg_bot/hendlers.py
+++ tg_bot/hendlers.py
@@ -1,9 +1,21 @@
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardButton, WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer(f"Привет, {message.from_user.username}")
+    builder = InlineKeyboardBuilder()
+    text = ["Проверить баланс"]
+    web_app = WebAppInfo(url="https://telegram.org/apps")
+    for i in text:
+        builder.add(InlineKeyboardButton(
+            text=i, 
+            web_app=web_app)
+    )
+    await message.answer(text=f"Привет, {message.from_user.first_name}",
+                         reply_markup=builder.as_markup()
+    )
+    
