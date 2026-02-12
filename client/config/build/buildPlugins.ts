4fa7859943102c9125f98601f30af865ff454ae1
Rostislaw Chernyahovskiy <r.chernyahovskll@gmail.com>
2026-02-12 10:18:04 +0300

    fixed bugs with deleting files from git

--- client/config/build/buildPlugins.ts
+++ client/config/build/buildPlugins.ts
@@ -2,10 +2,18 @@ import { BuildOptions } from './types/types';
 import { Configuration } from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import { PassThrough } from 'stream';
 
-export function buildPlugins({paths}: BuildOptions): Configuration["plugins"] {
-    return [
-        new HtmlWebpackPlugin({ template: paths.html }),
+export function buildPlugins({paths, mode}: BuildOptions): Configuration["plugins"] {
+    const plugins = [new HtmlWebpackPlugin({ template: paths.html })]
+
+    if (mode === "development") {
         new webpack.ProgressPlugin()
-    ]
+    }
+
+    if (mode === "production") {
+        ;
+    }
+
+    return plugins
 }
